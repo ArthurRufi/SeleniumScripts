be491ae6a0af55e72caa017b7004766290c39307
ArthurRufi <temparthur87@gmail.com>
2025-07-22 22:15:20 -0300

    not first now kk

--- recap.py
+++ recap.py
@@ -1,22 +1,13 @@
-#recaptular alguns conceitos e tecnicas do selenium com python
-
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
-
-
-# isso vai inicializar o navegador
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
-
-#isso vai abrir o site
-driver.get("https://www.google.com")
-
-#fazendo pesquisa
-search_box = driver.find_element(by=By.NAME, value="q")
-search_box.send_keys("Asimov Academy")
-search_box.submit()
-
-driver.quit()
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
